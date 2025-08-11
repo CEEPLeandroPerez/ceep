94e8ec9cdd120fec7e68d4ce191a29069733014b
CEEPLeandroPerez <leandro.perez@escola.pr.gov.br>
2025-08-11 10:56:43 -0300

    Add files via upload

--- CALCULADORA DE IMC.py
+++ CALCULADORA DE IMC.py
@@ -1,8 +1,10 @@
 peso = float(input("Digite o seu peso em kg: "))
 altura = float(input("Digite a sua altura em metros: "))
 
-imc = peso / (altura ** 2)
+if altura > 3:
+    altura = altura / 100
 
+imc = peso / (altura ** 2)
 print(f"Seu IMC é: {imc:.2f}")
 
 if imc < 18.5:
