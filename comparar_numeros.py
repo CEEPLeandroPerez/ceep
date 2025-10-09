25474a76f153de496aeac674a070cd5613412ca2
CEEPLeandroPerez <leandro.perez@escola.pr.gov.br>
2025-10-09 08:26:29 -0300

    Add files via upload

---
+++ comparar_numeros.py
@@ -0,0 +1,11 @@
+print("vamos comparar dois mumeros!")
+num1 = int(input("por-favor informe o 1º numero!"))
+num2 = int(input("por-favor informe o 2º numero!"))
+
+if num1 > num2:
+    print(f"o numero {num1} e maior que o {num2}")
+
+elif num1 < num2:
+    print(f"o numero {num1} e menor que o {num2}")
+else:
+    print("os dois numeros sao iguais")
---
+++ verificar_idade_para_votar.py
@@ -0,0 +1,7 @@
+print("vamos verificar se vc pode votar?")
+idade=int(input("me informe sua idade:"))
+
+if idade >16:
+    print("voce ja pode votar.")
+else:
+    print("voce ainda nao pode votar.")    
